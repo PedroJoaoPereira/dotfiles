508ff716d9932ae57d0ebb548af9044d0a21de62
PEDRO SOARES JOAO PEREIRA <pedro.pereira@mercedes-benz.io>
2024-02-29 18:00:37 +0000

    initial config with scripting

---
+++ install/install_zsh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# holds the directory of the script caller
+scriptPath="${BASH_SOURCE[0]:-$0}"
+# solves the case of the symlink
+while [ -h "$scriptPath" ]; do
+    cd "$(dirname -- "$scriptPath")"
+    scriptPath="$(readlink -f -- "$scriptPath")"
+done
+
+# remove old configurations - this is dangerous if no backups
+# for me if it is not checked in, it is not important
+echo "install_zsh >> Removing old configurations"
+rm -rf ~/.zshrc* ~/.oh-my-zsh
+
+# stow the zshrc from the parent directory of this script
+echo "install_zsh >> Stowing zshrc"
+(cd "$(dirname -- "$scriptPath")/.." && stow -t ~ zshrc/)
+
+# install oh-my-zsh without overriding the stow'ed zshrc
+echo "install_zsh >> Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
